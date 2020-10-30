a1a5b190dd55f3b254d92f90d9528c304125a735
magnustiberius <bgonzales20001@gmail.com>
2020-10-29 23:27:21 -0500

    schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,83 +1,42 @@
-drop table if exists USERS;
-drop table if exists country;
-drop table if exists inventory;
-drop table if exists delivery_header;
-drop table if exists delivery_details;
-drop table if exists inventory_ledger_quantity;
-drop table if exists inventory_ledger_amount;
+drop table if exists pooltable;
+drop table if exists rental_transaction;
+drop table if exists bill_charge;
 
-create table USERS(
-  ID int not null AUTO_INCREMENT,
-  NAME varchar(100) not null,
-  STATUS int,
-  PRIMARY KEY ( ID )
-);
 
-CREATE TABLE country (
-  id   INTEGER      NOT NULL AUTO_INCREMENT,
-  name VARCHAR(128) NOT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE inventory (
+CREATE TABLE pooltable (
   id   			INTEGER      NOT NULL AUTO_INCREMENT,
   code 			VARCHAR(128) NOT NULL,
   name 			VARCHAR(128) NOT NULL,
   desc 			VARCHAR(128) NOT NULL,
-  unit_price 		FLOAT,
-  qty_in_stock 		FLOAT,
-  unit_weight 		VARCHAR(128),
-  reorder_level 	FLOAT,
-  reorder_time_in_days 	FLOAT,
-  reorder_qty   	FLOAT,
-  discontinued  	BOOL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE inventory_ledger_quantity (
-  id   			INTEGER      NOT NULL AUTO_INCREMENT,
-  code 			VARCHAR(128) NOT NULL,
-  name 			VARCHAR(128) NOT NULL,
-  ref 			VARCHAR(128) NOT NULL,
-  debit 		FLOAT,
-  credit 		FLOAT,
-  post_date 	DATE,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE inventory_ledger_amount (
-  id   			INTEGER      NOT NULL AUTO_INCREMENT,
-  code 			VARCHAR(128) NOT NULL,
-  name 			VARCHAR(128) NOT NULL,
-  ref 			VARCHAR(128) NOT NULL,
-  debit 		FLOAT,
-  credit 		FLOAT,
-  post_date 	DATE,
   PRIMARY KEY (id)
 );
 
 
-CREATE TABLE delivery_header (
+CREATE TABLE rental_transaction (
   id   			INTEGER      NOT NULL AUTO_INCREMENT,
-  vendor_id		INTEGER      NOT NULL,
-  vendor_name 	VARCHAR(128) NOT NULL,
-  receive_date 	DATE,
-  user_id		INTEGER      NOT NULL,
-  posted_flag	BOOL,
-  post_date 	DATE,
+  pooltable_id  INTEGER NOT NULL,
+  start_time 	DATE,
+  end_time 		DATE,
+  rate_per_hour FLOAT,
+  posted_flag  	BOOL,
+  posted		DATE,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE delivery_details (
+
+CREATE TABLE bill_charge (
   id   			INTEGER      NOT NULL AUTO_INCREMENT,
-  header_id		INTEGER      NOT NULL,
-  inventory_id	INTEGER      NOT NULL,
-  code 			VARCHAR(128) NOT NULL,
-  name 			VARCHAR(128) NOT NULL,
-  desc 			VARCHAR(128) NOT NULL,
-  unit_price 	FLOAT,
-  unit_weight 	VARCHAR(128),
-  posted_flag	BOOL,
-  post_date 	DATE,
+  rental_txn_id INTEGER,
+  bill_date 	DATE,
+  amount_charge FLOAT,
+  discount      FLOAT,
+  tip           FLOAT,
+  amount_paid   FLOAT,
+  posted_flag  	BOOL,
+  posted		DATE,
   PRIMARY KEY (id)
+);
+
+
+
 );
